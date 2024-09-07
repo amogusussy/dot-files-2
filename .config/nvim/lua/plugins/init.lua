a825aa2484e00f5cca296d8d2f86b1817e7121ab
amogussussy <amogussussy@protonmail.com>
2024-09-07 22:29:03 +0100

    update dots

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -276,6 +276,22 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+
+  -- {
+  --   "OXY2DEV/markview.nvim",
+  --   lazy = false,      -- Recommended
+  --   -- ft = "markdown" -- If you decide to lazy-load anyway
+  --
+  --   dependencies = {
+  --       -- You will not need this if you installed the
+  --       -- parsers manually
+  --       -- Or if the parsers are in your $RUNTIMEPATH
+  --       "nvim-treesitter/nvim-treesitter",
+  --
+  --       "nvim-tree/nvim-web-devicons"
+  --   }
+  -- }
+
 }
 
 local config = require("core.utils").load_config()
