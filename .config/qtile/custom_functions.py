f4b42024797e87e03954a575c59f8effe977f582
amogusussy <amogussussy@protonmail.com>
2025-01-06 19:24:15 +0000

    update dots

--- .config/qtile/custom_functions.py
+++ .config/qtile/custom_functions.py
@@ -5,8 +5,6 @@ from libqtile.lazy import lazy
 def minimize_monitors(qtile, all=False):
     groups = qtile.groups if all else [qtile.current_group]
     for group in groups:
-        if not group.screen:
-            continue
         for win in group.windows:
             if hasattr(win, "toggle_minimize"):
                 win.toggle_minimize()
