aa4673557cea175c832e76b8196af36b90ec6caa
amogusussy <amogussussy@protonmail.com>
2025-02-08 15:07:58 +0000

    update dots

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,11 +5,13 @@ picom --config $HOME/.config/picom/picom.conf &
 xset r rate 200 50
 
 sleep 2
-# Reverse audio.
-pactl load-module module-remap-sink \
-  sink_name=reverse-stereo \
-  master=1 \
-  channels=2 \
-  master_channel_map=front-right,front-left \
-  channel_map=front-left,front-right
-pactl set-default-sink reverse-stereo
+
+if [[ "$(pactl get-default-sink)" == "reverse-stereo" ]]; then
+    printf "Audio already reversed\n";
+    return
+fi;
+sink_number="$(pactl list short sinks | grep "RUNNING" | awk '{print $1}')";
+$output="$(pactl load-module module-remap-sink "sink_name=reverse-stereo" master="$sink_number" channels=2 master_channel_map=front-right,front-left channel_map=front-left,front-right)";
+if [[ "$?" == "0" ]]; then
+    pactl set-default-sink "reverse-stereo";
+fi
